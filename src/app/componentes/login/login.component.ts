2c13d4557795f5f9e2ea4d5a605a687dd0fe125f
gabi <gabriela.paliuco@shift.com.br>
2025-08-15 14:11:30 -0300

    Criação da tela de Login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  camposFormulario = [ 'email', 'telefone'];
+
+  constructor(private router: Router) {}
+
+  login(dados: any) {
+    console.log('Dados de login: ', dados);
+    this.router.navigate(['/cadastro'])
+  }
 
 }
